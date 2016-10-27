445592bbfdb9e310a3f080a2cf27319d35fcd245
cwytko <cwytko@users.noreply.github.com>
2016-10-26 18:11:10 -0700

    Update main.c
    Updating main to include the source

--- main.c
+++ main.c
@@ -1,10 +1,24 @@
-while(1)
-{
-  int i = 0;
-  i++;
-  printf("This is how many shits i give about this code: 0); 
-  printf("printed %d times", i);
-  printf("Zeid was here\n");
-  system("pause");
+#include <stdio.h>
 
+int _2spooky4u = 10;
+
+int main void()
+{
+    int i = 0;
+  
+  while(1)
+  {
+  
+    i++;
+    printf("This is how many shits i give about this code: 0); 
+    printf("printed %d times", i);
+    printf("Zeid was here\n");
+    // system("pause");
+    getchar();
+    
+    printf("The global int yo: %d\n", _2spooky4u);
+
+  }
+           
+           return 0;
 }
